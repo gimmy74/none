e97a3cad151bf3a4ff185f8ad43cc31d1f1ebcee
gimmy74 <72813434+gimmy74@users.noreply.github.com>
2021-10-30 19:13:30 -0400

    .

--- box.js
+++ box.js
@@ -9,7 +9,8 @@ var blookFunction = () => {
     document.getElementsByClassName('styles__boxHeader___UoHkq-camelCase')[0].innerText = 'Red box';
     document.getElementsByClassName('styles__mysteryBoxIcon___18nWF-camelCase')[0].style = `color: #0000ff;`;
     document.getElementsByClassName('styles__mysteryBoxIcon___18nWF-camelCase')[0].style = `background: #000fff;`;
-    document.getElementsByClassName('styles__boxTokenText___NdiWk-camelCase')[0].innerText = `250`;        
+    document.getElementsByClassName('styles__boxTokenText___NdiWk-camelCase')[0].innerText = `250`; 
+    document.getElementsByClassName('styles__blookContainer___2Yq1S-camelCase')[0].src = 'https://cdn.discordapp.com/emojis/796921352774877224.png?size=96';       
     
     
 }
