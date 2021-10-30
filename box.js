8ea425e2068afba1b90fc79f079627fe28d081e1
gimmy74 <72813434+gimmy74@users.noreply.github.com>
2021-10-30 18:30:03 -0400

    .

--- box.js
+++ box.js
@@ -7,9 +7,9 @@ var blookFunction = () => {
 const tokencost = (prompt(`How much do you want the box to be?`))
 
 
-    document.getElementsByClassName('styles__boxHeaderText___UoHkq-camelCase').innerText = boxname;
-    document.getElementsByClassName('styles__mysteryBoxIcon___18nWF-camelCase').style = `color: #${color};`;
-    document.getElementsByClassName('styles__boxTokenText___1c1GF-camelCase').innerText = tokencost;        
+    document.getElementsByClassName('styles__boxHeaderText___UoHkq-camelCase')[0].innerText = boxname;
+    document.getElementsByClassName('styles__mysteryBoxIcon___e-Gwl-camelCase')[0].style = `color: #${color};`;
+    document.getElementsByClassName('styles__boxTokenText___1c1GF-camelCase')[0].innerText = tokencost;        
     
     
 }
