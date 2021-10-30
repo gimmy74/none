b5ae9d8493a2d41987a3420e8993153d5ca29294
gimmy74 <72813434+gimmy74@users.noreply.github.com>
2021-10-30 18:56:24 -0400

    .

---
+++ archive.js
@@ -0,0 +1,4 @@
+
+    const boxname = (prompt(`What do you want the box name do be?`));
+const color = (prompt(`What color do you want the box to be?`));
+const tokencost = Number(prompt(`How much do you want the box to be?`));
--- box.js
+++ box.js
@@ -2,9 +2,7 @@ const nothing = document.getElementsByClassName('styles__miniBlook___3eAjl-camel
 const nothing2 = document.getElementsByClassName('styles__blookContainer___GKC0D-camelCase')
 const nothing3 = document.getElementsByClassName('styles__box___2pZ5d-camelCase')
 
-const boxname = (prompt(`What do you want the box name do be?`));
-const color = (prompt(`What color do you want the box to be?`));
-const tokencost = Number(prompt(`How much do you want the box to be?`));
+
 
 var blookFunction = () => {
  
