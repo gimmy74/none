e3e5df6c99471e82f18d85e11e7e5f7ce8977ac2
gimmy74 <72813434+gimmy74@users.noreply.github.com>
2021-10-30 18:09:47 -0400

    .

--- money.js
+++ money.js
@@ -1,6 +1,6 @@
 
 var blookFunction = () => {
     const tokens = Number(prompt(`How many tokens do you want?`))
-    document.getElementsByClassName('styles__tokenBalanceText___2eYY7-camelCase')[0].src = tokens;
+    document.getElementsByClassName('styles__tokenBalanceText___2eYY7-camelCase').innerText = tokens;
 }
 blookFunction();
