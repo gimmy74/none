720fa33ea84a7fdf00342078a99d09c4ae1cced9
gimmy74 <72813434+gimmy74@users.noreply.github.com>
2021-10-29 23:35:58 -0400

    .

--- blood.js
+++ blood.js
@@ -3,7 +3,7 @@ const blookname = 'Blood Frog';
 var blookFunction = () => {
     document.getElementsByClassName('styles__blook___2Yq1S-camelCase')[0].src = blook;
     document.getElementsByClassName('styles__priceContainer___1pnjg-camelCase')[0].children[0].innerText = 'Sell Price: Not Sellable';
-    document.getElementsByClassName('styles__quantityText___JDfUO-camelCase')[0].innerText = 'Quantitiy: 5';
+    document.getElementsByClassName('styles__quantityText___JDfUO-camelCase')[0].innerText = 'Quantity: 5';
     document.getElementsByClassName('styles__rarityText___3sXH2-camelCase')[0].innerText = 'Debug';
     document.getElementsByClassName('styles__rarityText___3sXH2-camelCase')[0].style = 'color: #000001;';
     document.getElementsByClassName('styles__headerTextContainer___xVg-I-camelCase')[0].children[0].innerText = blookname;
--- poggies.js
+++ poggies.js
@@ -3,7 +3,7 @@ const blookname = 'Troll';
 var blookFunction = () => {
     document.getElementsByClassName('styles__blook___2Yq1S-camelCase')[0].src = blook;
     document.getElementsByClassName('styles__priceContainer___1pnjg-camelCase')[0].children[0].innerText = 'Sell Price: 1000000';
-    document.getElementsByClassName('styles__quantityText___JDfUO-camelCase')[0].innerText = 'Quantitiy: 2346';
+    document.getElementsByClassName('styles__quantityText___JDfUO-camelCase')[0].innerText = 'Quantity: 2346';
     document.getElementsByClassName('styles__rarityText___3sXH2-camelCase')[0].innerText = 'Troll';
     document.getElementsByClassName('styles__rarityText___3sXH2-camelCase')[0].style = 'color: #0000ff;';
     document.getElementsByClassName('styles__headerTextContainer___xVg-I-camelCase')[0].children[0].innerText = blookname;
