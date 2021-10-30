ee6415f134c2a8290c9055238fb30401d43e369b
gimmy74 <72813434+gimmy74@users.noreply.github.com>
2021-10-30 19:02:21 -0400

    .

--- box.js
+++ box.js
@@ -3,13 +3,13 @@ const nothing2 = document.getElementsByClassName('styles__blookContainer___GKC0D
 const nothing3 = document.getElementsByClassName('styles__box___2pZ5d-camelCase')
 
 
-
 var blookFunction = () => {
  
 
     document.getElementsByClassName('styles__boxHeader___UoHkq-camelCase')[0].innerText = 'Red box';
     document.getElementsByClassName('styles__mysteryBoxIcon___18nWF-camelCase')[0].style = `color: #0000ff;`;
-    document.getElementsByClassName('styles__boxTokenText___NdiWk-camelCase')[0].innerText = `25`;        
+    document.getElementsByClassName('styles__mysteryBoxIcon___18nWF-camelCase')[0].style = `background: #000fff;`;
+    document.getElementsByClassName('styles__boxTokenText___NdiWk-camelCase')[0].innerText = `250`;        
     
     
 }
