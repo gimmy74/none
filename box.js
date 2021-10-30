059976e7c22e44bca64a01747a8595866f4b8549
gimmy74 <72813434+gimmy74@users.noreply.github.com>
2021-10-30 18:04:24 -0400

    .

--- box.js
+++ box.js
@@ -1,4 +1,5 @@
-
+const nothing = document.getElementsByClassName('styles__miniBlook___3eAjl-camelCase');
+const nothing2 = document.getElementsByClassName('styles__blookContainer___GKC0D-camelCase')
 var blookFunction = () => {
     const boxname = (prompt(`What do you want the box name do be?`))
     const color = (prompt(`What color do you want the box to be?`))
@@ -6,12 +7,8 @@ const tokencost = (prompt(`How much do you want the box to be?`))
     document.getElementsByClassName('styles__box___2pZ5d-camelCase')
     document.getElementsByClassName('styles__boxHeader___UoHkq-camelCase').innerText = boxname;
     document.getElementsByClassName('styles__mysteryBoxIcon___18nWF-camelCase').style = `color: #${color};`;
-    document.getElementsByClassName('styles__boxTokenContainer___1c1GF').innerText = tokencost;
-    document.getElementsByClassName('styles__blookContainer___GKC0D-camelCase')
-    document.getElementsByClassName('styles__miniBlook___3eAjl-camelCase');
-        
+    document.getElementsByClassName('styles__boxTokenContainer___1c1GF').innerText = tokencost;        
     
-
     
 }
 blookFunction();
