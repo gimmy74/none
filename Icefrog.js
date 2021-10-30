ef90fba2fad7d99d4e44c0cdd381ede348b2a8f4
gimmy74 <72813434+gimmy74@users.noreply.github.com>
2021-10-30 15:22:48 -0400

    .

--- Icefrog.js
+++ Icefrog.js
@@ -2,7 +2,7 @@ const blook = 'https://media.discordapp.net/attachments/903063371425906708/90408
 const blookname = 'Ice Frog';
 var blookFunction = () => {
     document.getElementsByClassName('styles__blook___2Yq1S-camelCase')[0].src = blook;
-    document.getElementsByClassName('styles__priceContainer___1pnjg-camelCase')[0].children[0].innerText = 'Sell Price: Not Sellable';
+    document.getElementsByClassName('styles__priceContainer___1pnjg-camelCase')[0].children[0].innerText = 'Sell Price: 300';
     document.getElementsByClassName('styles__quantityText___JDfUO-camelCase')[0].innerText = 'Quantity: 1';
     document.getElementsByClassName('styles__rarityText___3sXH2-camelCase')[0].innerText = 'Chroma';
     document.getElementsByClassName('styles__rarityText___3sXH2-camelCase')[0].style = 'color: #5AC9F9;';
