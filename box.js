ebd1941b1d4dd3609f831c2acf97c8ea8117ed87
gimmy74 <72813434+gimmy74@users.noreply.github.com>
2021-10-30 18:50:07 -0400

    .

--- box.js
+++ box.js
@@ -1,15 +1,17 @@
 const nothing = document.getElementsByClassName('styles__miniBlook___3eAjl-camelCase');
 const nothing2 = document.getElementsByClassName('styles__blookContainer___GKC0D-camelCase')
 const nothing3 = document.getElementsByClassName('styles__box___2pZ5d-camelCase')
-var blookFunction = () => {
-    const boxname = (prompt(`What do you want the box name do be?`));
-    const color = (prompt(`What color do you want the box to be?`));
+
+const boxname = (prompt(`What do you want the box name do be?`));
+const color = (prompt(`What color do you want the box to be?`));
 const tokencost = Number(prompt(`How much do you want the box to be?`));
 
+var blookFunction = () => {
+ 
 
-    document.getElementsByClassName('styles__boxHeader___UoHkq-camelCase')[0].children[0].innerText = boxname;
-    document.getElementsByClassName('styles__mysteryBoxContainer___18nWF-camelCase')[0].children[0].style = `color: #${color};`;
-    document.getElementsByClassName('styles__boxTokenContainer___NdiWk-camelCase')[0].childeren[0].innerText = tokencost;        
+    document.getElementsByClassName('styles__boxHeader___UoHkq-camelCase')[0].children[0].innerText = 'Red box';
+    document.getElementsByClassName('styles__mysteryBoxContainer___18nWF-camelCase')[0].children[0].style = `color: #0000ff;`;
+    document.getElementsByClassName('styles__boxTokenContainer___NdiWk-camelCase')[0].childeren[0].innerText = `25`;        
     
     
 }
