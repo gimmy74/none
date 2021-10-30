bb6807a67b89361d8a3574d8712c007f9075a217
gimmy74 <72813434+gimmy74@users.noreply.github.com>
2021-10-30 19:51:42 -0400

    .

--- blook.js
+++ blook.js
@@ -1,15 +1,15 @@
 const nothing = 'https://media.discordapp.net/attachments/903063371425906708/904087321002008616/unknown.png_1.png';
 
 
-
+const name = (prompt(`What do you want the blook name to be?`))
+const url = (prompt(`What do you want your blook to look like(URL only)`))
+const quantity = Number(prompt(`What quanity(number) of the blook do you want?`));
+const sellprice = (prompt(`What Number do you want the sell price to be?`));
+const rarity = (prompt(`What Rarity do you want the blook to be?`));
+const color = (prompt(`What color do you want the rarity to be(Dont include the hashtag)`));
 
 var blookFunction = () => {
-    const name = (prompt(`What do you want the blook name to be?`))
-    const url = (prompt(`What do you want your blook to look like(URL only)`))
-    const quantity = Number(prompt(`What quanity(number) of the blook do you want?`));
-    const sellprice = (prompt(`What Number do you want the sell price to be?`));
-    const rarity = (prompt(`What Rarity do you want the blook to be?`));
-    const color = (prompt(`What color do you want the rarity to be(Dont include the hashtag)`));
+
     
     document.getElementsByClassName('styles__blook___2Yq1S-camelCase')[0].src = url;
     document.getElementsByClassName('styles__priceContainer___1pnjg-camelCase')[0].children[0].innerText = `Sell Price: ${sellprice}`;
@@ -33,53 +33,6 @@ var onClick = () => {
 try { 
     blookFunction();
     onClick();
-    onClick();
-    onClick();
-    onClick();
-    onClick();
-    onClick();
-    onClick();
-    onClick();
-    onClick();
-    onClick();
-    onClick();
-    onClick();
-    onClick();
-    onClick();
-    onClick();
-    onClick();
-    onClick();
-    onClick();
-    onClick();
-    onClick();
-    onClick();
-    onClick();
-    onClick();
-    onClick();
-    onClick();
-    onClick();
-    onClick();
-    onClick();
-    onClick();
-    onClick();
-    onClick();
-    onClick();
-    onClick();
-    onClick();
-    onClick();
-    onClick();
-    onClick();
-    onClick();
-    onClick();
-    onClick();
-    onClick();
-    onClick();
-    onClick();
-    onClick();
-    onClick();
-    onClick();
-    onClick();
-    
     } catch (error) {
         alert(error)
     }
