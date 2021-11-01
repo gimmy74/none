1146ca46276344552b6438a517dc7e79af6022d8
gimmy74 <72813434+gimmy74@users.noreply.github.com>
2021-10-31 22:29:32 -0400

    .

--- box.js
+++ box.js
@@ -1,26 +1,3 @@
-const nothing = document.getElementsByClassName('styles__miniBlook___3eAjl-camelCase');
-const nothing2 = document.getElementsByClassName('styles__blookContainer___GKC0D-camelCase')
-const nothing3 = document.getElementsByClassName('styles__box___2pZ5d-camelCase')
-
-
-var blookFunction = () => {
- 
-
-    document.getElementsByClassName('styles__boxHeader___UoHkq-camelCase')[0].innerText = 'Red box';
-    document.getElementsByClassName('styles__mysteryBoxIcon___18nWF-camelCase')[0].style = `color: #0000ff;`;
-    document.getElementsByClassName('styles__mysteryBoxIcon___18nWF-camelCase')[0].style = `background: #000fff;`;
-    document.getElementsByClassName('styles__boxTokenText___NdiWk-camelCase')[0].innerText = `250`; 
-    document.getElementsByClassName('styles__miniBlookContainer___1Y4oH-camelCase')[0].style = 'background: #000fff;';       
-    document.getElementsByClassName('styles__box___2pZ5d-camelCase')[0].style = 'background: #000fff;';
-    document.getElementsByClassName('styles__box___2pZ5d-camelCase')[0].style = 'color: #000fff;';
-    document.getElementsByClassName('styles__blook___2Yq1S-camelCase')[0].src = 'https://cdn.discordapp.com/emojis/796921352774877224.png?size=96'
-    
-}
-try { 
-    blookFunction();
-    } catch (error) {
-        alert(error)
-    }
     var blookInfo = {
         Blooks: {
             Witch: {
