8237a6a99f71891f58d701f068ff04c6c27f6fca
gimmy74 <72813434+gimmy74@users.noreply.github.com>
2021-11-03 10:16:43 -0400

    Update blook.js

--- blook.js
+++ blook.js
@@ -11,18 +11,14 @@ const color = (prompt(`What color do you want the rarity to be(Dont include the 
 var blookFunction = () => {
 
     
-    document.getElementsByClassName('styles__blook___2Yq1S-camelCase')[0].src = url;
-    document.getElementsByClassName('styles__priceContainer___1pnjg-camelCase')[0].children[0].innerText = `Sell Price: ${sellprice}`;
-    document.getElementsByClassName('styles__quantityText___JDfUO-camelCase')[0].innerText = `Quantity: ${quantity}`;
-    document.getElementsByClassName('styles__rarityText___3sXH2-camelCase')[0].innerText = rarity;
-    document.getElementsByClassName('styles__rarityText___3sXH2-camelCase')[0].style = `color:` + `#${color}`;
-    document.getElementsByClassName('styles__headerTextContainer___xVg-I-camelCase')[0].children[0].innerText = name;
-    document.getElementsByClassName('styles__blookContainer___GKC0D-camelCase')[0].src = url;
+    var o = document.getElementsByClassName("styles__blookArrayContainer___9Ae0L-camelCase")[0];
+o.innerHTML = "";
+    t = `<button onclick="${`() => \n        pass\n        document.getElementsByClassName('styles__blook___2Yq1S-camelCase')[0].src = '${a}';\n        document.getElementsByClassName('styles__priceContainer___1pnjg-camelCase')[0].children[0].innerText = 'Sell Price: ${sellprice}';\n        document.getElementsByClassName('styles__quantityText___JDfUO-camelCase')[0].innerText = 'Quantity: ${quanity}';\n        document.getElementsByClassName('styles__rarityText___3sXH2-camelCase')[0].innerText = '${rarity}';\n        document.getElementsByClassName('styles__rarityText___3sXH2-camelCase')[0].style = 'color: #${color};';\n        document.getElementsByClassName('styles__headerTextContainer___xVg-I-camelCase')[0].children[0].innerText = '${url}';\n    `}"\n role="button"tabindex="0"style="font-size: 0px; outline: none; user-select: none; margin: 5px 1vw; position: relative; border: none; backface-visibility: hidden; background-color: transparent;">\n<div class="styles__blookContainer___GKC0D-camelCase"\n style="z-index: 1; margin: 0px auto; width: 5vw; height: 5.75vw; cursor: pointer; position: relative; outline: none;">\n<img src="${name}"alt="${url} Blook"draggable="false"class="styles__blook___2Yq1S-camelCase"></div>\n</button>`;
+    o.innerHTML += t
 }
 
 try { 
     blookFunction();
-    onClick();
     } catch (error) {
         alert(error)
     }
