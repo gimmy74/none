529623a1599b12f415910917e6e1a7fef1cfbfa2
gimmy74 <72813434+gimmy74@users.noreply.github.com>
2021-11-04 07:12:26 -0400

    Update money.js

--- money.js
+++ money.js
@@ -3,8 +3,5 @@ var Tokens = () => {
     const tokens = (prompt(`How many tokens do you want?`))
     document.getElementsByClassName('styles__tokenBalanceText___2eYY7-camelCase')[0].innerText = `${tokens}`;
 } 
-try { 
-    Tokens();
-    } catch (error) {
-        alert(error)
-    }
+
+ Tokens();
