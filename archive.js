dcde79a7d0c066bbdf69819d0411ee2ba7117fac
gimmy74 <72813434+gimmy74@users.noreply.github.com>
2021-12-15 22:38:18 -0500

    .

--- archive.js
+++
@@ -1,4 +0,0 @@
-
-    const boxname = (prompt(`What do you want the box name do be?`));
-const color = (prompt(`What color do you want the box to be?`));
-const tokencost = Number(prompt(`How much do you want the box to be?`));
