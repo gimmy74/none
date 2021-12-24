e8718dec619898ef14a97dd61b5abda057f5ecf0
gimmy74 <72813434+gimmy74@users.noreply.github.com>
2021-12-24 16:50:54 -0500

    Update smth.js

--- smth.js
+++ smth.js
@@ -2,7 +2,7 @@ const cmd = prompt('What would you like to run?')
 if(cmd == 'auto') {
     setInterval(() => { 
       Game.shimmers.forEach((_, i) => Game.shimmers[i].pop()) 
-      }, 500)
+      }, 1)
   
     setInterval(() => {
        Game.ClickCookie(); 
